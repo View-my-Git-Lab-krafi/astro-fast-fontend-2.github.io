53c411036a441f8c7240daec8b45e9d7fc3087d6
Your Name <you@example.com>
2024-07-03 02:25:45 +0100

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://krafi.gitlab.io',
-  base: '/astro-fast-fontend-1',
-  outDir: 'public',
-  publicDir: 'static',
-});
+  site: 'https://astro-fast-fontend-2.github.io',
+})
