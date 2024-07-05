e726138fbf1dd4152e06ca33d0a6f80bd0ca5b95
Your Name <you@example.com>
2024-07-05 10:50:02 +0100

    /astro-fast-fontend-2.github.io

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  base: '/astro-fast-fontend-2.github.io',
   site: 'https://astro-fast-fontend-2.github.io',
 })
+
+
+//If you are using netlify
+//export default defineConfig({ output: "server", adapter: netlify() });
+
+
+//if you are using localhost PC
+//export default defineConfig({}); 
